4049f321a043522956b2e8717f0073a0f962e35b
maybeags <suren0709@gmail.com>
2025-01-07 11:49:07 +0900

    feat: 20250107 Constructor Manual Writing

--- src/main/java/chapter09_classes/products/ProductMain.java
+++ src/main/java/chapter09_classes/products/ProductMain.java
@@ -35,4 +35,31 @@ package chapter09_classes.products;
         productName GFlip6
  */
 public class ProductMain {
+    public static void main(String[] args) {
+        // 객체 생성
+        // 기본 생성자 이용한 객체 생성
+        Product product1 = new Product();
+        product1.productNum = 123456;
+        product1.productName = "LG엘패드";
+
+        // 매개 변수 생성자를 이용한 객체 생성
+        Product product2 = new Product(987654);
+        product2.productName = "다이소충전기";
+
+        Product product3 = new Product("USB-C 케이블");
+        product3.productNum = 159357;
+
+        Product product4 = new Product(951753, "GFlip6");
+
+        product1.showInfo1();
+        product2.showInfo1();
+        product3.showInfo1();
+        product4.showInfo1();
+
+        System.out.println();
+        System.out.println(product1.showInfo2());
+        System.out.println(product2.showInfo2());
+        System.out.println(product3.showInfo2());
+        System.out.println(product4.showInfo2());
+    }
 }
