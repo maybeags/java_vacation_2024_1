b5c81623dc756d40c1626892e8c81a7e31313f0e
maybeags <suren0709@gmail.com>
2025-01-09 09:58:48 +0900

    feat: UnivStudent / UnivStudentMain.java added

--- src/main/java/chapter10_setter_getter/univ_student/UnivStudent.java
+++ src/main/java/chapter10_setter_getter/univ_student/UnivStudent.java
@@ -36,6 +36,76 @@ package chapter10_setter_getter.univ_student;
  */
 public class UnivStudent {
     // 필드
+    //    1. 클래스 설계
+    //        field 종류 : name / grade / score (score는 double) -> 근데 전부 private
+    private String name;
+    private int grade;
+    private double score;
 
+    // 여기까지 작성했다고 가정했을 때 -> 기본 생성자는 일단 default -> Main만들어서
 
+    // alt + insert 사용 -> 생성자 5개
+
+    public UnivStudent() {
+    }
+
+    public UnivStudent(String name) {
+        this.name = name;
+    }
+
+    public UnivStudent(int grade) {
+        this.grade = grade;
+    }
+
+    public UnivStudent(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public UnivStudent(String name, int grade, double score) {
+        this.name = name;
+        this.grade = grade;
+        this.score = score;
+    }
+
+    // 3. setter / getter : alt + insert
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {           // set 조건 필요
+        if (grade > 0 && grade < 5){
+            this.grade = grade;
+        } else {
+            System.out.println("잘못된 학년을 입력하셨습니다.");
+        }
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {        // set 조건 필요
+        if (score >= 0 && score <= 4.5){
+            this.score = score;
+        } else {
+            System.out.println("잘못된 점수를 입력하셨습니다.");
+        }
+    }
+
+    public void showInfo() {
+        System.out.println("이름 : " + this.name);
+        System.out.println("학년 : " + this.grade + " 학년");
+        System.out.println("점수 : " + this.score);
+        System.out.println();
+    }
 }
