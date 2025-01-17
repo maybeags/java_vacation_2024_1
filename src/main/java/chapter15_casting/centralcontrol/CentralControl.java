de92717f123c86a076d80751b3e64c66b19ed077
maybeags <suren0709@gmail.com>
2025-01-17 11:46:20 +0900

    feat : 20250117 reference casting

--- src/main/java/chapter15_casting/centralcontrol/CentralControl.java
+++ src/main/java/chapter15_casting/centralcontrol/CentralControl.java
@@ -20,11 +20,15 @@ public class CentralControl {
         int emptyIndex = checkEmpty();  // checkEmpty()의 리턴값이 addDevice에서 이용되는 함수형 프로그래밍
 
         if(emptyIndex == -1) {  // java에서는 마이너스인덱스 개념이 없기 때문에 checkEmpty()
-                                // 메서드에서 -1을 반환시킨 이유는
-                                // 비어있는 배열이 없다는 의미이기 때문에 다음과 같이 조건식을 설정했습니다.
+            // 메서드에서 -1을 반환시킨 이유는
+            // 비어있는 배열이 없다는 의미이기 때문에 다음과 같이 조건식을 설정했습니다.
             System.out.println("더 이상 장치를 연결할 수 없습니다.");
             return;             // return; : 해당 메서드를 종료한다는 의미.
         }
+        deviceArray[emptyIndex] = device;
+        System.out.println(device.getClass().getSimpleName() + " 클래스의 객체가 추가되었습니다.");
+        // 객체명.getClass() -> 패키지 명을 포함한 클래스명이 출력됨
+        // 객체명.getClass().GetSimpleName() -> 클래스명만 출력
     }
     public int checkEmpty() {   // 비어있는 배열의 index를 반환하도록 할 것이기 때문에 return type int
         // 그리고 그 비어있는 배열의 index에 addDevice()를 통해
@@ -38,4 +42,48 @@ public class CentralControl {
         }
         return -1;
     }
+
+    // 배열 내부에 있는 element들의 전원을 전부 켜고 끄는 메서드를 구현할 예정
+
+    public void powerOn() {
+        // 배열 내부에 있는 요소들(객체들 -> Power의 서브클래스들의 객체)의 공통적인 메서드인
+        // .on()을 실행시킬 예정입니다.
+        // 해당 경우에 생겨날 수 있는 잠재적 문제점 :
+        //      배열의 특정 인덱스 내에 아무런 객체가 없는 상황이라면 .on()을 적용 시켰을 때,
+        //      컴파일링 에러가 발생할 확률이 있으므로, 이를 고려한 프로그래밍을 작성할 예정.
+
+        for (int i = 0 ;  i < deviceArray.length ; i++) {
+            if(deviceArray[i] == null) {
+                //특정 인덱스 넘버의 element가 null이라면
+                continue;   // 해당 반복문은 종료하고 다음 인덱스로 넘긴다는 의미
+            }
+            deviceArray[i].on();    // continue가 실행되지 않았다면 해당 라인의 코드가 실행됨.
+        }
+    }
+
+    public void powerDown() {
+        // powerDown을 위의 powerOn()을 참조하여 작성하는데,
+        // 향상된 for문을 적용하여 작성하시오.
+        // for ( 자료형 변수명 : 배열명(여기서만) ) {
+            //구현부
+    //  }
+        for (Power device : deviceArray) {
+            // 만약에 null이라면 continue가 적용
+            if(device == null) {
+                continue;
+            }
+            device.off();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/chapter15_casting/centralcontrol/Main.java
+++ src/main/java/chapter15_casting/centralcontrol/Main.java
@@ -12,7 +12,40 @@ public class Main {
         Computer computer1 = new Computer();
         LED led1 = new LED();
         Speaker speaker1 = new Speaker();
-        CentralControl centralControl = new CentralControl(new Power[3]);   // 이거 interfaces에서 배운 방식인데
+        Speaker speaker2 = new Speaker();           // 실패 사례용
+
+        CentralControl centralControl = new CentralControl(new Power[6]);   // 이거 interfaces에서 배운 방식인데
         // 생성자의 argument로 'new 클래스()' 즉, 생성자를 집어넣은 케이스를 한 번 더 응용해서 걔가 배열이기까지 합니다.
+
+
+        centralControl.addDevice(computer1);
+        centralControl.addDevice(led1);
+        centralControl.addDevice(speaker1);
+        centralControl.addDevice(speaker2);         // 실패 사례용으로 코드 하나 작성했습니다.
+
+//        speaker2.on();
+//        speaker2.off();
+//        System.out.println("↑얘는 배열에 포함 안된 애입니다.");
+        centralControl.powerOn();
+        System.out.println("↓끄는 부분");
+        centralControl.powerDown();
+
+        /*
+            20250120 수업 시작시 예정 사항
+            1. CentralControl에 displayinfo()를 call1() 타입으로 정의해서
+                centralControl 객체 내에 있는 필드인 deviceArray에 들어있는 element를 조회할 수 있도록
+                코드를 작성하시오.
+
+            2. Tv 클래스 및 Mouse 클래스를 작성하고
+                Main 단계에서 centralControl 객체에 집어넣으시고,
+                .powerOn() 시켜보세요.
+
+            3. 이상까지 하게되면 Computer/Speaker/LED/Tv/Mouse라는 각 객체가 Power로 암시적 업캐스팅을 하여
+                오버라이드된 메서드들을 일괄적으로 실행시킨 사례에 대한 공부가 끝이 납니다.
+                다음에 제 수업 시작시에는 명시적 다운 캐스팅을 통해 각 클래스의 객체들이 가지고 있는
+                고유 메서드들을 실행시킬 수 있도록 프로그램을 버전업할 예정입니다.
+
+         */
+
     }
 }
